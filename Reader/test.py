29c1fb35212637c76113164ebf708cec2b331a5f
Brian Terrell <terrelbr@oregonstate.edu>
2020-11-12 22:06:12 -0700

    Push to git hub for remote work - not complete

--- Reader/test.py
+++ Reader/test.py
@@ -25,10 +25,25 @@ def ConfigBusTesting():
     else:
         ConfigBus("start", True, None)
 
+# Temp command to sent updates to UI
+def UpdateValue(PGN, SPN, value):
+    log.debug("PGN=%d SPN=%d Value=%d", PGN, SPN, value)
+    message = dict(PGN=dict(id=PGN), SPNArry=[dict(id=SPN, currentVal=value)])
+    #print(message)
+    pub.sendMessage('UpdateValue', payload=message)
+
+
+def UpdateValueTesting():
+    value = input("What command do you want to test? ")
+    if value == "1":
+        UpdateValue(65265, 69, 1)
+    if value == "2":
+        UpdateValue(65265, 69, 0)
 
 def TestMain():
     # Below are tests that are in the main loop, only one should be active at a time, the rest should be commented out.
     while True:
         # ReceiverConfig command testing
-        ConfigBusTesting()
+        #ConfigBusTesting()
+        UpdateValueTesting()
         
