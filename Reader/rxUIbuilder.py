5c92c1baff6d8cd63cbc718de1eac177647db378
Brian Terrell <terrelbr@oregonstate.edu>
2020-11-16 18:14:53 -0700

    UI rx working with test msgs

--- Reader/rxUIbuilder.py
+++ Reader/rxUIbuilder.py
@@ -3,9 +3,7 @@ import tkinter as tk
 from tkinter import ttk
 import tkinter.font as tkFont
 from pubsub import pub
-from pprint import pprint
-import configVerif as cv
-import conversion as con
+
 
 class simulatorWindow:
 	
@@ -32,24 +30,16 @@ class simulatorWindow:
 		self.mainFrame = ttk.Frame(self.parent, padding=(10,10))
 		self.mainFrame.pack()
 		
-		#if cv.verifPGN(fM.get_PGUDict(self.configDict)):
 		titleFont = tkFont.Font(family="Helvetica",size=36,weight="bold")
 		self.titleFrame = ttk.Frame(self.mainFrame, padding=(30,0))
-		ttk.Label(self.titleFrame, text='J1939 Simulator Sender', font = titleFont).pack(expand=1, fill=tk.X)
+		ttk.Label(self.titleFrame, text='J1939 Simulator Receiver', font = titleFont).pack(expand=1, fill=tk.X)
 		self.titleFrame.pack()
-			#self.parent.bind_all("<1>", lambda event:event.widget.focus_set())
+		
 		self.__SimTitle()
 		self.__BuildPGURows()
 		self.__BuildStopStart()
 		pub.subscribe(self.__UpdateValue, "UpdateValue")
 
-
-		#else:
-		#	titleFont = tkFont.Font(family="Helvetica",size=18,weight="bold")
-		#	self.titleFrame = ttk.Frame(self.mainFrame, padding=(30,0))
-		#	ttk.Label(self.titleFrame, text='Configuration file error, please check error log.', font = titleFont).pack(expand=1, fill=tk.X)
-		#	self.titleFrame.pack()
-		
 
 
 	'''
@@ -103,7 +93,6 @@ class simulatorWindow:
 				SPNFrame = tk.LabelFrame(PGUFrame, text=  PGU["SPNDict"][SPNkey]["Label"] + " - " + str(SPNkey), font=lblFrmFont)
 				SPNFrame.pack(padx=5, pady=10, side=tk.LEFT, fill=tk.Y)
 				self.__BuildParamValue(PGU["SPNDict"][SPNkey], SPNFrame)
-				#self.__BuildModEntry(PGU["SPNDict"][SPNkey], SPNFrame, PGU)
 
 
 	'''
@@ -147,64 +136,6 @@ class simulatorWindow:
 
 
 	'''
-    	Name: 	BuildModEntry
-    	Desc:   Creates the SPN modifier entry window in the SPN widget
-    	Param:  SPN - dictonary object contians SPN data,
-    			SPNFrame - tkinter frame to contain SPN,
-    			PGU - dictonary object contians PGU data
-    	Return: None
-	'''	
-	#def __BuildModEntry(self, SPN, SPNFrame, PGU):
-	#	if SPN["simMutable"] == True:
-	#		modFont = tkFont.Font(family="Helvetica",size=14,weight="bold")
-	#		entryframe= ttk.Frame(SPNFrame, padding=(5,5))
-	#		entryframe.pack()
-	#		SPN["UI_Objects"]["entryVal"] = tk.StringVar()
-	#		SPN["UI_Objects"]["entry"] = tk.Entry(entryframe, textvariable=SPN["UI_Objects"]["entryVal"])
-	#		SPN["UI_Objects"]["entry"].grid(row=1, column=1, sticky="ew")
-	#		SPN["UI_Objects"]["entry"].bind("<1>", lambda event: self.__EntryUpdateBinding(SPN, SPNFrame, PGU))
-
-
-	'''
-    	Name: 	EntryUpdateBinding
-    	Desc:   Creates the SPN modifier entry event bindings
-    	Param:  SPN - dictonary object contians SPN data,
-    			SPNFrame - tkinter frame to contain SPN,
-    			PGU - dictonary object contians PGU data
-    	Return: None
-	'''	
-	#def __EntryUpdateBinding(self, SPN, SPNFrame, PGU):
-	#	SPN["UI_Objects"]["entry"].config(foreground='black')
-	#	SPNFrame.configure(bd=4)
-	#	SPN["UI_Objects"]["entry"].bind("<Return>", lambda event: self.parent.focus_set())
-	#	SPN["UI_Objects"]["entry"].bind("<FocusOut>", lambda event: self.__EntryUpdateAccept(SPN, SPNFrame, PGU))
-
-
-	'''
-    	Name: 	EntryUpdateAccept
-    	Desc:   Verifies user entry and provides feedback in the case of invalid entry. 
-    	Param:  SPN - dictonary object contians SPN data,
-    			SPNFrame - tkinter frame to contain SPN,
-    			PGU - dictonary object contians PGU data
-    	Return: None
-	'''	
-	#def __EntryUpdateAccept(self, SPN, SPNFrame, PGU):
-	#	SPNFrame.configure(bd=2)
-	#	newEntry = SPN["UI_Objects"]["entry"].get()
-	#	
-	#	if con.StrIsFloat(newEntry) and float(newEntry) >= SPN["loLimit"] and float(newEntry) <= SPN["hiLimit"]:
-	#		SPN["UI_Objects"]["currentVal"] = float(newEntry)				
-	#		if SPN["resolution"] < 1:
-	#			newEntry = round(float(newEntry), 2)										
-	#		newEntry = str(newEntry)  + ' ' + SPN["unit"]
-	#		SPN["UI_Objects"]["dispValLbl"].config(text=newEntry)
-	#		self.__SPNUpdateMsg(PGU, SPN, SPN["UI_Objects"]["currentVal"])
-	#		SPN["UI_Objects"]["entry"].delete(0, 'end')
-	#	else:
-	#		SPN["UI_Objects"]["entry"].config(foreground='red')
-
-
-	'''
 	    Name: BuildStopStart  
 	    Desc: Create the start and stop simulation buttons in the mainFrame of the UI   
 	    Param:  None
@@ -222,8 +153,8 @@ class simulatorWindow:
 
 	'''
 	    Name: StartSim  
-	    Desc: 
-	    Param:  None
+	    Desc: Sets button appearance for start stop button in the start mode
+	    Param:  startStopBtn tkinter button object
 	    Return: None
 	'''
 	def __StartSim(self, startStopBtn):
@@ -235,8 +166,8 @@ class simulatorWindow:
 
 	'''
 	    Name: StartSim  
-	    Desc: 
-	    Param:  None
+	    Desc: Sets button appearance for start stop button in the stop mode
+	    Param:  startStopBtn tkinter button object
 	    Return: None
 	'''
 	def __StopSim(self, startStopBtn):
@@ -254,7 +185,6 @@ class simulatorWindow:
 	'''
 	def __StartSimMsg(self):
 		message = dict([("status", "start"), ("logging", False), ("loggingFileName","")])
-		#pprint(message)
 		pub.sendMessage('ReceiverConfig', payload=message)
 	
 
@@ -266,7 +196,6 @@ class simulatorWindow:
 	'''
 	def __StopSimMsg(self):
 		message = dict([("status", "stop"), ("logging", False), ("loggingFileName","")])
-		#pprint(message)
 		pub.sendMessage('ReceiverConfig', payload=message)
 
 
@@ -322,19 +251,6 @@ class simulatorWindow:
 
 
 	'''
-	    Name: 	SPNUpdateMsg
-	    Desc:   Passes a message down to update a specific SPN 
-	    Param:  SPN - dictonary object contians SPN data, 
-	    		PGU - dictonary object contians PGU data,
-	    		newValue - new SPN value to be updated
-	    Return: None
-	'''
-	def __SPNUpdateMsg(self, PGU, SPN, newValue):
-		message = self.__UpdateDictComposer(PGU, SPN, newValue)
-		pub.sendMessage('SPNValueUpdate', payload=message)
-
-
-	'''
 	    Name: 	intDictComposer
 	    Desc:   Builds init message with new value to pub/sub to builder layer
 	    Param:  PGUDict - dictonary object contianing all PGU data
@@ -363,20 +279,16 @@ class simulatorWindow:
 	def __initSimMsg(self, PGUDict):
 		message = self.__intDictComposer(PGUDict)
 		pub.sendMessage('InitRead', payload=message)
-		#pprint(message)
+
 
 	'''
-	    Name: 	UpdateListener
+	    Name: 	UpdateValue
 	    Desc:   Passes a message down to initialize a specific SPN    
 	    Param:  PGUDict - dictonary object contianing all PGU data
 	    Return: None
 	'''
 	def __UpdateValue(self, payload=None):
 		PGN = payload["PGN"]["id"]
-		#print(self.PGUDict)
 		SPNDict = self.PGUDict[str(PGN)]["SPNDict"]
-		#print(PGN)
 		for SPN in payload['SPNArry']:
-			#print(SPNDict[str(SPN['id'])]['id']) 
 			SPNDict[str(SPN['id'])]["UI_Objects"]["dispValLbl"].config(text=str(SPN["currentVal"]))
-			#print(SPN)
